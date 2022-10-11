532fe425dc08014f6a27a7281f79061d22acfb40
gabrielteixeira-0814 <gabrielteixeira0814@gmail.com>
2022-10-10 21:00:38 -0300

    Editando role

--- app/Http/Controllers/Acl/RoleController.php
+++ app/Http/Controllers/Acl/RoleController.php
@@ -37,28 +37,9 @@ class RoleController extends Controller
         return $role = Role::find($id);
     }
 
-    // public function update($id)
-    // {
-    //     $role = Role::find($id);
-    //     $permission = Permission::get();
-    //     $rolePermissions = DB::table("role_has_permissions")->where("role_has_permissions.role_id",$id)
-    //         ->pluck('role_has_permissions.permission_id','role_has_permissions.permission_id')
-    //         ->all();
-    
-    //     return view('roles.edit',compact('role','permission','rolePermissions'));
-    // }
-
     public function update(Request $request)
     {
-        // if($request->ajax()){
-            
-        //     $role = Role::find($request['id']);
-
-        //     return  $role;
-        //     if($role) {
-        //         $role->update($request);
-        //         return $role;
-        //     }
-        // }
+        $role = Role::find($request['id']);
+        return   $role->update(['name' => $request['name']]);
     }
 }
